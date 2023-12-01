e796ac37188fde70d6f9cccd7002917e4dda8949
myhappydays <seokjun0515@gmail.com>
2023-12-01 16:41:33 +0900

    계산식을 사용함. *실패

--- BEAKJOON/2563.py
+++ BEAKJOON/2563.py
@@ -2,13 +2,13 @@ I = int(input())                            #색종이의 수
 Paper_Arr = [[0, 0] for i in range(I)]      #색종이의 위치 *2차원 정수 배열
 for i in range(I):
     Paper_Arr[i][0], Paper_Arr[i][1] = map(int, input().split())
-Black = 0
+Black = 100 * I
+Black2 = 0
 
-for i in range(max([Paper_Arr[a][0] for a in range(I)]) + 10):     #색종이의 세로 정점
-    for j in range(max([Paper_Arr[b][0] for b in range(I)]) + 10):     #색종이의 가로 정점
-        for k in range(I):
-            if Paper_Arr[k][0] <= i >= Paper_Arr[k][0] + 10 and Paper_Arr[k][1] <= j >= Paper_Arr[k][1] + 10:
-                Black += 1
-                break
+for a, i in enumerate(Paper_Arr):
+    for b, j in enumerate(Paper_Arr):
+        if j[0] <= i[0] and i[0] >= j[0] + 10:
+            Black2 += (10 - abs(i[0] - j[0])) * (10 - abs(i[1] - j[1]))
 
+Black -= int(Black2 / 2)
 print(Black)
