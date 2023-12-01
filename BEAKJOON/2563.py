c6329d57efd22ac55d6bbed9da21cbbadc37a8f1
myhappydays <seokjun0515@gmail.com>
2023-12-01 16:57:03 +0900

    백준 2563번 색종이

--- BEAKJOON/2563.py
+++ BEAKJOON/2563.py
@@ -1,14 +1,10 @@
-I = int(input())                            #색종이의 수
-Paper_Arr = [[0, 0] for i in range(I)]      #색종이의 위치 *2차원 정수 배열
-for i in range(I):
-    Paper_Arr[i][0], Paper_Arr[i][1] = map(int, input().split())
-Black = 100 * I
-Black2 = 0
+Paper = [[0 for _ in range(101)] for _ in range(101)]
+I = int(input())
+Color_Paper = [list(map(int, input().split())) for _ in range(I)]
 
-for a, i in enumerate(Paper_Arr):
-    for b, j in enumerate(Paper_Arr):
-        if j[0] <= i[0] and i[0] >= j[0] + 10:
-            Black2 += (10 - abs(i[0] - j[0])) * (10 - abs(i[1] - j[1]))
+for i in Color_Paper:
+    for a in range(i[0], i[0] + 10):
+        for b in range(i[1], i[1] + 10):
+            Paper[a][b] = 1
 
-Black -= int(Black2 / 2)
-print(Black)
+print(sum([Paper[i].count(1) for i in range(101)]))
