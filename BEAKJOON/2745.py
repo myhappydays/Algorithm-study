bc17c2d24a5be3de71aee100e0a6827f22a362d1
myhappydays <seokjun0515@gmail.com>
2023-12-05 16:03:40 +0900

    백준 2745번 진법 변환

--- BEAKJOON/2745.py
+++ BEAKJOON/2745.py
@@ -1,9 +1,2 @@
-N, B = input().split()
-N = N[::-1]                                             #앞자리부터 계산하기 위한 슬라이싱
-B = int(B)
-Num = 0                                                 #10진수 변환값
-for i in range(len(N)):
-    ascii = ord(N[i])                                   #ord == string to askii
-    decimal = ascii-48 if ascii < 65 else ascii-55      #ascii 코드의 문자 '0' == 48, 문자 'A' == '65'이기에 각각 0, 10으로 만들었음.
-    Num += decimal * (B ** i)
-print(Num)
+A=list(input().split())
+print(int(A[0],int(A[1])))
