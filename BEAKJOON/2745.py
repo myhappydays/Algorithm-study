845f65f06434349797dd5e8c9e92a58ae8b64767
myhappydays <seokjun0515@gmail.com>
2023-12-05 16:00:14 +0900

    백준 2745번 진법 변환

--- BEAKJOON/2745.py
+++ BEAKJOON/2745.py
@@ -1,8 +1,9 @@
-Alphabet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 N, B = input().split()
-N = N[::-1]                                 #앞자리부터 계산하기 위한 슬라이싱
+N = N[::-1]                                             #앞자리부터 계산하기 위한 슬라이싱
 B = int(B)
-Num = 0                                     #10진수 변환값
+Num = 0                                                 #10진수 변환값
 for i in range(len(N)):
-    Num += Alphabet.index(N[i]) * (B ** i)  #알파벳 리스트에서의 순서로 10진수로 변환
+    ascii = ord(N[i])                                   #ord == string to askii
+    decimal = ascii-48 if ascii < 65 else ascii-55      #ascii 코드의 문자 '0' == 48, 문자 'A' == '65'이기에 각각 0, 10으로 만들었음.
+    Num += decimal * (B ** i)
 print(Num)
