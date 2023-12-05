a996b4973f262b8422619ee37d2ebd537cb38e30
myhappydays <seokjun0515@gmail.com>
2023-12-05 15:33:48 +0900

    백준 2745번 진법 변환

--- BEAKJOON/2745.py
+++ BEAKJOON/2745.py
@@ -1,8 +1,8 @@
-Alphabet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'Y', 'X', 'Y', 'Z']
+Alphabet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 N, B = input().split()
-N = N[::-1]
+N = N[::-1]                                 #앞자리부터 계산하기 위한 슬라이싱
 B = int(B)
-Num = 0
+Num = 0                                     #10진수 변환값
 for i in range(len(N)):
-    Num += Alphabet.index(N[i]) * (B ** i)
+    Num += Alphabet.index(N[i]) * (B ** i)  #알파벳 리스트에서의 순서로 10진수로 변환
 print(Num)
